54a09043b1d835aacac274ce04c97a3d2a8f5358
Giles Thomas <giles@giles.net>
2013-01-31 14:24:22 +0000

    Initial unit test based on node-lame's first one.

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
+// Developed with help from https://github.com/TooTallNate/node-lame/blob/master/test/decoder.js
+
+var fs = require("fs");
+
 var flac = require("./flac");
 
-exports.testCanConstruct = function(test){
-    test.expect(1);
+exports.testCanConstruct = function (test){
     var decoder = flac.Decoder();
-    test.ok(true, "this assertion should pass");
     test.done();
+};
+
+
+exports.testEmitsOneOrMoreReadableEvents = function (test) {
+    var file = fs.createReadStream("testaudio/tone16bit.flac");
+    var count = 0;
+    var decoder = new flac.Decoder();
+    decoder.on("readable", function () {
+        count ++;
+        var b;
+        while (null != (b = decoder.read())) ;
+    });
+    decoder.on('finish', function () {
+        test.ok(count > 0, "Did not get more than zero bytes");
+        test.done();
+    });
 };
